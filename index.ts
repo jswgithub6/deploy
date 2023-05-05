a4e4ee3f87b8b4cae786adad3125229c567b48ef
wjs <1530466272@qq.com>
2023-05-05 18:54:25 +0800

    feat: v1.0.0 实现将本地的输出构建文件上传至服务器的功能

---
+++ index.ts
@@ -0,0 +1,64 @@
+import dotenv from "dotenv"
+import ora from "ora"
+import chalk from "chalk"
+import tar from "tar"
+import { NodeSSH, Config } from "node-ssh"
+import { join } from "path/posix"
+import { readdirSync } from "fs"
+
+dotenv.config()
+
+const config: Config = {
+  host: process.env.SSH_HOST,
+  username: process.env.SSH_USER,
+  port: Number(process.env.SSH_PORT),
+  password: process.env.SSH_PASSWORD,
+}
+
+const ssh = new NodeSSH()
+
+async function deploy() {
+  try {
+    const dist = process.env.DIST as string
+    const files = readdirSync(dist)
+    const filename = 'dist.tgz'
+    const RemoteDir = process.env.REMOTE_UPLOAD_DIR as string
+    const publicDir = join(RemoteDir, '..')
+    const spinner = ora().start()
+    // 压缩文件
+    spinner.text = `${chalk.green('Compressing file...')}`
+    await tar.create({
+      cwd: dist,
+      file: filename
+    }, files)
+    spinner.succeed(chalk.green('File compressed successfully!'))
+
+    // ssh连接服务器
+    spinner.text = `${chalk.green('Connecting server...')}`
+    await ssh.connect(config)
+    spinner.succeed(chalk.green("Connection successful"))
+
+    // 上传文件
+    spinner.text = `${chalk.green('Uploading file...')}`
+    await ssh.putFile(filename, join(publicDir, filename))
+    spinner.succeed(`${chalk.green('File uploaded successfully!')}`)
+
+    // 删除服务端的旧文件
+    spinner.text = `${chalk.green('Deleting files...')}`
+    await ssh.execCommand(`rm -r ${join(RemoteDir, '/*')}`)
+    spinner.succeed(`${chalk.green('Files deleted.')}`)
+
+    // 解压文件
+    spinner.text = `${chalk.green('Extracting file...')}`
+    await ssh.execCommand(`tar xf ${filename} -C ${RemoteDir} && rm -f ${filename}`, {
+      cwd: publicDir
+    })
+    spinner.succeed(`${chalk.green('File extracted successfully!')}`)
+  } catch (error) {
+    console.log(chalk.red(error))
+  } finally {
+    ssh.dispose()
+  }
+}
+
+deploy()
